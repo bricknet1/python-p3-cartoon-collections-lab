4e86f4cffa6d27a71edb9be1ab8247028cae7e32
Nick Johnson <nlj222@gmail.com>
2023-03-09 10:34:29 -0800

    lab complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,29 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(stuff):
+    i = 1
+    for each in stuff:
+        print(f"{i}. {each}")
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(stuff):
+    printing = []
+    for each in stuff:
+        x = each.capitalize()
+        printing.append(f"{x}!")
+    return printing
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(stuff):
+    stuff.sort(key=len, reverse=True)
+    if len(stuff[0]) > 4:
+        return True
+    elif len(stuff[0]) <= 4:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(words):
+    if "cheddar" in words:
+        return "cheddar"
+    elif "gouda" in words:
+        return "gouda"
+    elif "camambert" in words:
+        return "camambert"
+    else:
+        return None
